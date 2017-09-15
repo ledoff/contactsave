70f261958bbfe63b39eda05dac3e9bab7dbaa422
a.drogozhilov <seekinalias@gmail.com>
2017-09-15 16:05:45 +0300

    prestart

---
+++ app/src/main/java/com/example/adrogozhilov/bonus/Devicename.java
@@ -0,0 +1,40 @@
+package com.example.adrogozhilov.bonus;
+
+import android.os.Build;
+import android.text.TextUtils;
+
+/**
+ * Created by a.drogozhilov on 14.09.2017.
+ */
+public class Devicename {
+    public static String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return capitalize(model);
+        }
+        return capitalize(manufacturer) + " " + model;
+    }
+
+    private static String capitalize(String str) {
+        if (TextUtils.isEmpty(str)) {
+            return str;
+        }
+        char[] arr = str.toCharArray();
+        boolean capitalizeNext = true;
+
+        StringBuilder phrase = new StringBuilder();
+        for (char c : arr) {
+            if (capitalizeNext && Character.isLetter(c)) {
+                phrase.append(Character.toUpperCase(c));
+                capitalizeNext = false;
+                continue;
+            } else if (Character.isWhitespace(c)) {
+                capitalizeNext = true;
+            }
+            phrase.append(c);
+        }
+
+        return phrase.toString();
+    }
+}
